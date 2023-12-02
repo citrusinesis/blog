dae458bc676c80153cf32d1a2efbd2090ca6d5a2
Jiho Song <jihojiho2003@gmail.com>
2023-12-02 16:49:01 +0900

    Update site configuration

--- src/config.ts
+++ src/config.ts
@@ -1,16 +1,16 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  website: "https://blog.citrus.name", // replace this with your deployed domain
+  author: "Jiho Song",
+  desc: "Blog of Jiho Song",
+  title: "CITRUS.LOG",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerPage: 3,
 };
 
-export const LOCALE = ["en-EN"]; // set to [] to use the environment default
+export const LOCALE = ["en-EN", "ko-KR"]; // set to [] to use the environment default
 
 export const LOGO_IMAGE = {
   enable: false,
